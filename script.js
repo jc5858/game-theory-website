c617b54a453d2b2375a9df08a7251ddc76c0d7fe
Jason Chien <145613645+jc5858@users.noreply.github.com>
2025-04-10 22:01:09 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file will hold interactive JavaScript functionality
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Game Theory Explorer is loaded and ready!');
+    
+    // You can add more interactive features here as you learn
+});
